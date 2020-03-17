a6379210f652b0b4ae8159408058907f4795cd45
ttand <wqazsedc@gmail.com>
2020-03-17 15:40:24 +0800

    clean

--- code.cpp
+++ code.cpp
@@ -4,15 +4,137 @@
 #include <memory>
 #include "NvTriStrip.h"
 
-#include <assimp/Importer.hpp> 
-#include <assimp/scene.h>     
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
 #include <assimp/postprocess.h>
 #include <assimp/cimport.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+    string runName;
+    vector<string> loadFilenames;
+    string FMT = "PNT";
+    int defaultFlags = aiProcess_FlipWindingOrder | aiProcess_Triangulate | aiProcess_PreTransformVertices | aiProcess_CalcTangentSpace | aiProcess_GenSmoothNormals;
+    bool noStrip = false;
+    for (int argi = 0; argi < argc; ++argi)
+    {
+        cout << argv[argi] << endl;
+        if (!argi)
+            runName = argv[argi];
+        else
+        {
+            if (strcmp(argv[argi], "-cs") == 0 ||
+                strcmp(argv[argi], "--cachesize") == 0)
+            {
+                ++argi;
+
+                int cachesize = 16;
+                int num = sscanf(argv[argi], "%d", &cachesize);
+                if (num != 1)
+                {
+                    cerr << "Error reading cache size for -cs argument" << endl;
+                    exit(-1);
+                }
+
+                //cerr << "Cache Size " << cachesize << endl;
+                SetCacheSize(cachesize);
+            }
+            if (strcmp(argv[argi], "-fm") == 0 ||
+                strcmp(argv[argi], "--fromat") == 0)
+            {
+                ++argi;
+                FMT = argv[argi];
+            }
+            else if (strcmp(argv[argi], "-ns") == 0 ||
+                     strcmp(argv[argi], "--nostrip") == 0)
+            {
+                noStrip = true;
+            }
+            else
+            {
+                loadFilenames.emplace_back(argv[argi]);
+            }
+        }
+    }
+    Assimp::Importer Importer;
+    for (auto &loadFilename : loadFilenames)
+    {
+        auto pScene = Importer.ReadFile(loadFilename, defaultFlags);
+        if (pScene)
+        {
+            for (unsigned int i = 0; i < pScene->mNumMeshes; i++)
+            {
+                std::vector<float> vertexBuffer;
+                std::vector<uint16_t> indexBuffer;
+
+                auto paiMesh = pScene->mMeshes[i];
+                auto vertexCount = paiMesh->mNumVertices;
+
+                aiColor3D pColor{};
+                pScene->mMaterials[paiMesh->mMaterialIndex]->Get(AI_MATKEY_COLOR_DIFFUSE, pColor);
+                const aiVector3D Zero3D{};
+                for (unsigned int vertIndex = 0; vertIndex < paiMesh->mNumVertices; ++vertIndex)
+                {
+                    for (auto fm : FMT)
+                    {
+                        switch (fm)
+                        {
+                        case 'P':
+                            if (paiMesh->HasPositions())
+                            {
+                                const auto &pPos = paiMesh->mVertices[vertIndex];
+                                vertexBuffer.push_back(pPos.x);
+                                vertexBuffer.push_back(-pPos.y);
+                                vertexBuffer.push_back(pPos.z);
+                            }
+                            break;
+                        case 'N':
+                            if (paiMesh->HasNormals())
+                            {
+                                const auto &pNormal = paiMesh->mNormals[vertIndex];
+                                vertexBuffer.push_back(pNormal.x);
+                                vertexBuffer.push_back(-pNormal.y);
+                                vertexBuffer.push_back(pNormal.z);
+                            }
+                            break;
+                        case 'T':
+                            if (paiMesh->HasTextureCoords(0))
+                            {
+                                const auto &pTexCoord = paiMesh->mTextureCoords[0][vertIndex];
+                                vertexBuffer.push_back(pTexCoord.x);
+                                vertexBuffer.push_back(pTexCoord.y);
+                            }
+                            break;
+                        case 'C':
+                            if (paiMesh->HasVertexColors(vertIndex))
+                            {
+                                const auto &pTangent = paiMesh->mTangents[vertIndex];
+                                vertexBuffer.push_back(pTangent.x);
+                                vertexBuffer.push_back(pTangent.y);
+                                vertexBuffer.push_back(pTangent.z);
+                            }
+                            break;
+                        case 'B':
+                            if (paiMesh->HasTangentsAndBitangents())
+                            {
+                                const auto &pTangent = paiMesh->mTangents[vertIndex];
+                                vertexBuffer.push_back(pTangent.x);
+                                vertexBuffer.push_back(pTangent.y);
+                                vertexBuffer.push_back(pTangent.z);
+                            }
+                            break;
+                        default:
+                        }
+                    }
+
+                    const auto *pBiTangent = (paiMesh->HasTangentsAndBitangents()) ? &(paiMesh->mBitangents[vertIndex]) : &Zero3D;
+                }
+            }
+        }
+    }
+
     vector<unsigned short> indices{1, 2, 3, 2, 3, 4, static_cast<unsigned short>(-1)};
     //Call NvTriStrip to generate the strips
     //PrimitiveGroup *prims;
