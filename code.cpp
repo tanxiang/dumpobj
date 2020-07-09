e12a32bb7bdcc0acb24b7c7fe6c4d74092860655
ttand <wqazsedc@gmail.com>
2020-07-09 12:02:31 +0800

    mt

--- code.cpp
+++ code.cpp
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
     //int defaultFlags = aiProcessPreset_TargetRealtime_MaxQuality;
     bool noStrip = false;
     float pointscale = 1.0f;
+
     for (int argi = 0; argi < argc; ++argi)
     {
         //cout << argv[argi] << endl;
@@ -59,7 +60,6 @@ int main(int argc, char *argv[])
                 ++argi;
                 pointscale = std::stof(argv[argi]);
             }
-
             else if (strcmp(argv[argi], "-ns") == 0 ||
                      strcmp(argv[argi], "--nostrip") == 0)
             {
@@ -100,6 +100,11 @@ int main(int argc, char *argv[])
             for (unsigned int materialIndex = 0; materialIndex < pScene->mNumMaterials; ++materialIndex)
             {
                 aiColor3D color{};
+
+                pScene->mMaterials[materialIndex]->Get(AI_MATKEY_COLOR_AMBIENT, color);
+                materialsBuff.emplace_back(color.r*0.3);
+                materialsBuff.emplace_back(color.g*0.3);
+                materialsBuff.emplace_back(color.b*0.3);
                 pScene->mMaterials[materialIndex]->Get(AI_MATKEY_COLOR_DIFFUSE, color);
                 materialsBuff.emplace_back(color.r);
                 materialsBuff.emplace_back(color.g);
@@ -108,17 +113,24 @@ int main(int argc, char *argv[])
                 materialsBuff.emplace_back(color.r);
                 materialsBuff.emplace_back(color.g);
                 materialsBuff.emplace_back(color.b);
+                float shininess;
+                pScene->mMaterials[materialIndex]->Get(AI_MATKEY_SHININESS,shininess);
+                materialsBuff.emplace_back(shininess);
             }
             string path = loadFilename+".material.bin";
             ofstream filebuffer{path, ios::out | ofstream::binary};
             filebuffer.write(reinterpret_cast<char *>(materialsBuff.data()), materialsBuff.size() * sizeof(decltype(materialsBuff)::value_type));
         }
+        vector<uint16_t> materials;
         for (unsigned int meshIndex = 0; meshIndex < pScene->mNumMeshes; ++meshIndex)
         {
             auto saveMesh = mode.add_meshs();
             std::vector<uint16_t> indexBuffer;
             {
                 auto paiMesh = pScene->mMeshes[meshIndex];
+                materials.emplace_back(paiMesh->mMaterialIndex);
+                cout <<"meshIndex"<<meshIndex<<":mMaterialIndex" << paiMesh->mMaterialIndex << '\n';
+
                 auto vertexCount = paiMesh->mNumVertices;
                 map<vector<float>, int> vertexMap;
                 vector<vector<float>> vertexbufferOrg;
@@ -135,7 +147,7 @@ int main(int argc, char *argv[])
                             {
                                 const auto &pPos = paiMesh->mVertices[vertIndex];
                                 vertexPoint.push_back(pPos.x * pointscale);
-                                vertexPoint.push_back(-pPos.y * pointscale);
+                                vertexPoint.push_back(pPos.y * pointscale);
                                 vertexPoint.push_back(pPos.z * pointscale);
                             }
                             break;
@@ -144,7 +156,7 @@ int main(int argc, char *argv[])
                             {
                                 const auto &pNormal = paiMesh->mNormals[vertIndex];
                                 vertexPoint.push_back(pNormal.x);
-                                vertexPoint.push_back(-pNormal.y);
+                                vertexPoint.push_back(pNormal.y);
                                 vertexPoint.push_back(pNormal.z);
                             }
                             break;
@@ -222,6 +234,16 @@ int main(int argc, char *argv[])
                 *saveMesh->mutable_vertices() = {vertexBuffer.begin(), vertexBuffer.end()};
                 for_each(indexBuffer.begin(), indexBuffer.end(), [&](auto &n) { n = indexMap[n]; });
             }
+
+            ostringstream spath;
+            spath << saveDir << "mesh_" <<  std::setw(3) << std::setfill('0') << meshIndex <<"_index_" << '0' << "_slist" << ".bin";
+            ofstream filebuffer{spath.str(), ios::out | ios::binary};
+            filebuffer.write(reinterpret_cast<const char *>(indexBuffer.data()), sizeof(unsigned short) * indexBuffer.size());
+            ostringstream sdpath;
+            sdpath << saveDir << "mesh_" <<  std::setw(3) << std::setfill('0') << meshIndex <<"_index_" << '0' <<  "_slist_draw" << ".bin";
+            ofstream filebufferdraw{sdpath.str(), ios::out | ios::binary};
+            array<uint32_t, 5> drawCmd{indexBuffer.size(), 1, 0, 0, 0};
+            filebufferdraw.write(reinterpret_cast<const char *>(drawCmd.data()), sizeof(decltype(drawCmd)::value_type) * drawCmd.size());
             unique_ptr<PrimitiveGroup[]> prims;
             unsigned short numprims;
             {
@@ -255,7 +277,10 @@ int main(int argc, char *argv[])
                 filebufferdraw.write(reinterpret_cast<const char *>(drawCmd.data()), sizeof(decltype(drawCmd)::value_type) * drawCmd.size());
             }
         }
-        string path = loadFilename + '.' + FMT + ".pt";
+        string path = loadFilename + ".idx.bin";
+        ofstream savemt{path, ios::out | ios::binary};
+        savemt.write(reinterpret_cast<const char *>(materials.data()), sizeof(decltype(materials)::value_type) * materials.size());
+        path = loadFilename + '.' + FMT + ".pt";
         ofstream savept{path, ios::out | ios::binary};
         mode.SerializePartialToOstream(&savept);
         checkf = path;
